67518a83fdf1eca44e0d105b03d8e3bc2e74383f
문성현 <forest7592@hanmail.net>
2023-03-14 15:35:55 +0900

    Fix: Fix entity(Inquiry)
    Former-commit-id: d17897a2673d174bcee96e4301a31b2a95afb242

--- backend/src/main/java/com/a604/cake4u/inquiry/entity/Inquiry.java
+++ backend/src/main/java/com/a604/cake4u/inquiry/entity/Inquiry.java
@@ -1,9 +1,20 @@
 package com.a604.cake4u.inquiry.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Inquiry {
 
     @Id
