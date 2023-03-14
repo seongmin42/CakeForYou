487bdf37e9eb3854200c9db9216c231eb8c4539d
배진호 <qowlsgh99@naver.com>
2023-03-14 12:29:40 +0900

    Feat: Add Swagger
    Former-commit-id: a5cf69ef946798b936aac76ba61d3753582a3075

---
+++ backend/src/main/java/com/a604/cake4u/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.a604.cake4u.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket SwaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(swaggerInfo()) //  API Docu 및 작성자 정보 매핑
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.a604.cake4u.controller"))
+                .paths(PathSelectors.any()) //  controller package 전부
+//                  .paths(PathSelectors.ant("/controller")) //  controller package만 택해서 할 수도 있다.
+                .build()
+                .useDefaultResponseMessages(false); //  기본 세팅값인 200, 401, 402, 403, 404를 사용하지 않는다.
+    }
+
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder().title("Spring API Documentation")
+                .description("앱 서버 API를 설명하기 위한 문서입니다.")
+                .version("1")
+                .build();
+    }
+}
