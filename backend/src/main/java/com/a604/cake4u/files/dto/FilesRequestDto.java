c3d86365e85380f958971bb59fcd73c80520d29f
문성현 <forest7592@hanmail.net>
2023-03-14 15:33:12 +0900

    Feat: Add Entity files(Files, Inquiry)
    Former-commit-id: 4103092f4b9cdf2f5f0f64ef1bbe953f6581c6fa

---
+++ backend/src/main/java/com/a604/cake4u/files/dto/FilesRequestDto.java
@@ -0,0 +1,12 @@
+package com.a604.cake4u.files.dto;
+
+public class FilesRequestDto {
+
+    private Long id;
+    private Orders orderSheet;
+    private Cake cake;
+    private Seller seller;
+    private String fileUri;
+    private String fileName;
+    public String fileType;
+}
---
+++ backend/src/main/java/com/a604/cake4u/inquiry/entity/Inquiry.java
@@ -0,0 +1,23 @@
+package com.a604.cake4u.inquiry.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+public class Inquiry {
+
+    @Id
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+    @Column(nullable = false)
+    private LocalDate createdAt;
+    // 문의를 작성한 회원(구매자)
+    @Column(nullable = false)
+    private Buyer buyer;
+    // 문의를 받는 판매자
+    @Column(nullable = false)
+    private Seller seller;
+}
