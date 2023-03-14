347d985131bf72494e645addb756883ec8105a34
배진호 <qowlsgh99@naver.com>
2023-03-14 16:13:31 +0900

    Feat: 크림맛, 시트모양, 시트맛, 시트크기 엔티티 JoinColumn에 referencedColumnName사용
    Former-commit-id: 3a702d0e6975b49e937992e104f08694d3c38200

--- backend/src/main/java/com/a604/cake4u/sheettaste/entity/SheetTaste.java
+++ backend/src/main/java/com/a604/cake4u/sheettaste/entity/SheetTaste.java
@@ -1,5 +1,6 @@
 package com.a604.cake4u.sheettaste.entity;
 
+import com.a604.cake4u.seller.entity.Seller;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
@@ -15,11 +16,9 @@ public class SheetTaste {
     @GeneratedValue(strategy = GenerationType.AUTO) //100시작 vs 오토생성
     private Long id;
 
-//    @OneToOne
-//    @Column(nullable = false)
-//    @JoinColumn(name = "id")
-//    @Column(name = "seller_id")
-//    private Seller seller;
+    @OneToOne
+    @JoinColumn(name = "seller_id", referencedColumnName = "id", nullable = false)
+    private Seller seller;
 
     @Builder.Default
     @ColumnDefault("0")
