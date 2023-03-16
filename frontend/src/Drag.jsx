b81756c984e31a0f46cb52f35168c01149bc5309
문성현 <forest7592@hanmail.net>
2023-03-16 17:05:30 +0900

    Feat: Add drag page
    Former-commit-id: c4e42ab096359c11e099d014ebe2cd6440ae16e7

--- frontend/src/Drag.jsx
+++ frontend/src/Drag.jsx
@@ -1,32 +1,51 @@
-import React, { useRef } from "react";
-import Konva from "konva";
-import { Stage, Layer, Circle, Image } from "react-konva";
-import { useImage } from "react-konva-utils";
+import React, { useState } from "react";
+import { Stage, Layer, Image, Rect, Text } from "react-konva";
 import Blue from "./assets/img/blue.png";
 import Pink from "./assets/img/pink.png";
-import Candle from "./assets/img/candle.png";
-
-function generateItems() {
-  const newItems = [];
-  for (let i = 0; i < 10; i += 1) {
-    newItems.push({
-      x: Math.random() * window.innerWidth,
-      y: Math.random() * window.innerHeight,
-      id: `node-${i}`,
-      color: Konva.Util.getRandomColor(),
-    });
-  }
-  return newItems;
-}
+import CreamBlue from "./assets/img/cream_blue.png";
+import CreamRed from "./assets/img/cream_red.png";
+import CreamWhite from "./assets/img/cream_white.png";
+import RoundCake from "./assets/img/round_cake.png";
+import HeartCake from "./assets/img/heart_cake.png";
 
 function Drag() {
-  const imageRef = useRef(null);
-  const pinkImageRef = useRef(null);
-  const candleImageRef = useRef(null);
-  const [items, setItems] = React.useState(generateItems());
-  const [image] = useImage(Blue);
-  const [pinkImage] = useImage(Pink);
-  const [candleImage] = useImage(Candle);
+  const [items, setItems] = useState([]);
+  const trashZone = {
+    x: 0,
+    y: 0,
+    width: 200,
+    height: 200,
+  };
+
+  const createImageObject = (src, width, height) => {
+    const img = new window.Image();
+    img.src = src;
+    const id = `image-${Date.now()}`;
+
+    img.onload = () => {
+      setItems((oldItems) =>
+        oldItems.map((item) => {
+          if (item.id === id) {
+            return {
+              ...item,
+              image: img,
+            };
+          }
+          return item;
+        })
+      );
+    };
+
+    return {
+      id,
+      type: "image",
+      image: null,
+      x: Math.random() * window.innerWidth,
+      y: Math.random() * window.innerHeight,
+      width,
+      height,
+    };
+  };
 
   const handleDragStart = (e) => {
     const id = e.target.name();
@@ -43,65 +62,127 @@ function Drag() {
     const dItems = items.slice();
     const dItem = items.find((i) => i.id === id);
     const index = items.indexOf(dItem);
-    dItems[index] = {
-      ...dItem,
-      x: e.target.x(),
-      y: e.target.y(),
-    };
+
+    const newX = e.target.x();
+    const newY = e.target.y();
+
+    if (
+      newX >= trashZone.x &&
+      newX <= trashZone.x + trashZone.width &&
+      newY >= trashZone.y &&
+      newY <= trashZone.y + trashZone.height
+    ) {
+      dItems.splice(index, 1);
+    } else {
+      dItems[index] = {
+        ...dItem,
+        x: e.target.x(),
+        y: e.target.y(),
+      };
+    }
     setItems(dItems);
   };
-  const handleLoad = () => {
-    // do something when the image loads
+
+  const addItem = (src, width, height) => {
+    setItems([...items, createImageObject(src, width, height)]);
   };
 
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        {items.map((item) => (
-          <Circle
-            key={item.id}
-            name={item.id}
-            draggable
-            x={item.x}
-            y={item.y}
-            fill={item.color}
-            radius={50}
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          addItem(Blue, 500, 500);
+        }}
+      >
+        Add Blue
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          addItem(Pink, 500, 500);
+        }}
+      >
+        Add Pink
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          addItem(CreamBlue, 50, 50);
+        }}
+      >
+        Add CreamBlue
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          addItem(CreamRed, 50, 50);
+        }}
+      >
+        Add CreamRed
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          addItem(CreamWhite, 50, 50);
+        }}
+      >
+        Add CreamWhite
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          addItem(RoundCake, 500, 500);
+        }}
+      >
+        Add RoundCake
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          addItem(HeartCake, 500, 500);
+        }}
+      >
+        Add HeartCake
+      </button>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          {items.map((item) => (
+            <Image
+              key={item.id}
+              name={item.id}
+              image={item.image}
+              x={item.x}
+              y={item.y}
+              width={item.width}
+              height={item.height}
+              draggable
+              onDragStart={handleDragStart}
+              onDragEnd={handleDragEnd}
+              color="red"
+            />
+          ))}
+          <Rect
+            x={trashZone.x}
+            y={trashZone.y}
+            width={trashZone.width}
+            height={trashZone.height}
+            stroke="black"
+            strokeWidth={2}
+            dash={[4, 4]}
           />
-        ))}
-        <Image
-          ref={imageRef}
-          image={image}
-          x={0}
-          y={0}
-          width={500}
-          height={500}
-          onLoad={handleLoad}
-          draggable
-        />
-        <Image
-          ref={pinkImageRef}
-          image={pinkImage}
-          x={0}
-          y={0}
-          width={400}
-          height={200}
-          onLoad={handleLoad}
-          draggable
-        />
-        <Image
-          ref={candleImageRef}
-          image={candleImage}
-          x={0}
-          y={0}
-          width={50}
-          height={300}
-          onLoad={handleLoad}
-          draggable
-        />
-      </Layer>
-    </Stage>
+          <Text
+            x={trashZone.x}
+            y={trashZone.y + trashZone.height / 2 - 10}
+            width={trashZone.width}
+            height={20}
+            text="Trash Zone"
+            align="center"
+            fontSize={16}
+          />
+        </Layer>
+      </Stage>
+    </div>
   );
 }
 
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,129 @@
+/* eslint-disable react/prop-types */
+// src/components/Sidebar.js
+import React from "react";
+import styled from "styled-components";
+
+const SidebarContainer = styled.div`
+  width: 300px;
+  height: 100%;
+  background-color: #f0f0f0;
+  position: fixed;
+  right: 0;
+  top: 0;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 10;
+`;
+
+const StyledImage = styled.img`
+  width: 100px;
+  height: 100px;
+  margin-bottom: 20px;
+  cursor: pointer;
+  border: 1px solid black;
+`;
+
+// eslint-disable-next-line react/prop-types
+function Sidebar({
+  onImageClick,
+  // Blue,
+  // Pink,
+  // CreamBlue,
+  // CreamRed,
+  CreamWhite,
+  RoundCake,
+  // HeartCake,
+  Char1,
+  Char2,
+  Char3,
+  Char4,
+  Char5,
+  Char6,
+  HappyBirthday,
+}) {
+  return (
+    <SidebarContainer>
+      {/* <StyledImage
+        src={Blue.src}
+        onClick={() => {
+          onImageClick(Blue.src, Blue.width, Blue.height);
+        }}
+      />
+      <StyledImage
+        src={Pink.src}
+        onClick={() => onImageClick(Pink.src, Pink.width, Pink.height)}
+      />
+      <StyledImage
+        src={CreamBlue.src}
+        onClick={() =>
+          onImageClick(CreamBlue.src, CreamBlue.width, CreamBlue.height)
+        }
+      />
+      <StyledImage
+        src={CreamRed.src}
+        onClick={() =>
+          onImageClick(CreamRed.src, CreamRed.width, CreamRed.height)
+        }
+      /> */}
+      <StyledImage
+        src={CreamWhite.src}
+        onClick={() =>
+          onImageClick(CreamWhite.src, CreamWhite.width, CreamWhite.height)
+        }
+      />
+
+      <StyledImage
+        src={RoundCake.src}
+        onClick={() =>
+          onImageClick(RoundCake.src, RoundCake.width, RoundCake.height)
+        }
+      />
+      {/* <StyledImage
+        src={HeartCake.src}
+        onClick={() =>
+          onImageClick(HeartCake.src, HeartCake.width, HeartCake.height)
+        }
+      /> */}
+      <StyledImage
+        src={Char1.src}
+        onClick={() => onImageClick(Char1.src, Char1.width, Char1.height)}
+      />
+      <StyledImage
+        src={Char2.src}
+        onClick={() => onImageClick(Char2.src, Char2.width, Char2.height)}
+      />
+      <StyledImage
+        src={Char3.src}
+        onClick={() => onImageClick(Char3.src, Char3.width, Char3.height)}
+      />
+      <StyledImage
+        src={Char4.src}
+        onClick={() => onImageClick(Char4.src, Char4.width, Char4.height)}
+      />
+      <StyledImage
+        src={Char5.src}
+        onClick={() => onImageClick(Char5.src, Char5.width, Char5.height)}
+      />
+      <StyledImage
+        src={Char6.src}
+        onClick={() => onImageClick(Char6.src, Char6.width, Char6.height)}
+      />
+      <StyledImage
+        src={HappyBirthday.src}
+        onClick={() =>
+          onImageClick(
+            HappyBirthday.src,
+            HappyBirthday.width,
+            HappyBirthday.height
+          )
+        }
+      />
+
+      {/* ...more images */}
+    </SidebarContainer>
+  );
+}
+
+export default Sidebar;
