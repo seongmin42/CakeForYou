b60bd8290900c412c3e81712b7c7c79c7c61383b
배진호 <qowlsgh99@naver.com>
2023-03-16 16:19:39 +0900

    Misc: 포트폴리오 제작중, fileTypeEnum제작
    Former-commit-id: eb63ef1f41566b605949d33fdfe9437726aa727f

--- backend/src/main/java/com/a604/cake4u/portfolio/controller/PortfolioController.java
+++ backend/src/main/java/com/a604/cake4u/portfolio/controller/PortfolioController.java
@@ -1,7 +1,9 @@
 package com.a604.cake4u.portfolio.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/portfolio")
 public class PortfolioController {
 }
