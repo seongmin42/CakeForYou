ebb84787dd9507a9962e993bb29efad5ca80a3c3
unknown <seongmin42@naver.com>
2023-03-16 16:14:23 +0900

    Misc: Revise sql
    Former-commit-id: ece39c9d0e45871ab80bd19db4653d98d99e7f24

--- frontend/src/Drag.jsx
+++ frontend/src/Drag.jsx
@@ -1,32 +1,33 @@
-import React, { useRef } from "react";
-import Konva from "konva";
-import { Stage, Layer, Circle, Image } from "react-konva";
+import React from "react";
+// import Konva from "konva";
+import { Stage, Layer, Image } from "react-konva";
 import { useImage } from "react-konva-utils";
 import Blue from "./assets/img/blue.png";
 import Pink from "./assets/img/pink.png";
 import Candle from "./assets/img/candle.png";
 
 function generateItems() {
+  const [image] = useImage(Blue);
+  const [pinkImage] = useImage(Pink);
+  const [candleImage] = useImage(Candle);
+  const imgList = [image, pinkImage, candleImage];
+
   const newItems = [];
-  for (let i = 0; i < 10; i += 1) {
+  for (let i = 0; i < 3; i += 1) {
     newItems.push({
       x: Math.random() * window.innerWidth,
       y: Math.random() * window.innerHeight,
       id: `node-${i}`,
-      color: Konva.Util.getRandomColor(),
+      image: imgList[i],
     });
   }
+  console.log(newItems);
   return newItems;
 }
 
 function Drag() {
-  const imageRef = useRef(null);
-  const pinkImageRef = useRef(null);
-  const candleImageRef = useRef(null);
   const [items, setItems] = React.useState(generateItems());
-  const [image] = useImage(Blue);
-  const [pinkImage] = useImage(Pink);
-  const [candleImage] = useImage(Candle);
+  console.log(items);
 
   const handleDragStart = (e) => {
     const id = e.target.name();
@@ -50,56 +51,24 @@ function Drag() {
     };
     setItems(dItems);
   };
-  const handleLoad = () => {
-    // do something when the image loads
-  };
 
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
         {items.map((item) => (
-          <Circle
+          <Image
             key={item.id}
             name={item.id}
+            image={item.image}
             draggable
             x={item.x}
             y={item.y}
-            fill={item.color}
-            radius={50}
+            width={500}
+            height={500}
             onDragStart={handleDragStart}
             onDragEnd={handleDragEnd}
           />
         ))}
-        <Image
-          ref={imageRef}
-          image={image}
-          x={0}
-          y={0}
-          width={500}
-          height={500}
-          onLoad={handleLoad}
-          draggable
-        />
-        <Image
-          ref={pinkImageRef}
-          image={pinkImage}
-          x={0}
-          y={0}
-          width={400}
-          height={200}
-          onLoad={handleLoad}
-          draggable
-        />
-        <Image
-          ref={candleImageRef}
-          image={candleImage}
-          x={0}
-          y={0}
-          width={50}
-          height={300}
-          onLoad={handleLoad}
-          draggable
-        />
       </Layer>
     </Stage>
   );
