7334f5f60fd525977d81694ceb0fb51dc2c1ac00
unknown <seongmin42@naver.com>
2023-03-17 17:58:38 +0900

    Misc: Save
    Former-commit-id: 1af8141e89f6a3d1c7601c01265bddcdc450dcd8

--- backend/src/main/java/com/a604/cake4u/config/SwaggerConfig.java
+++ backend/src/main/java/com/a604/cake4u/config/SwaggerConfig.java
@@ -1,34 +1,49 @@
 package com.a604.cake4u.config;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+
 public class SwaggerConfig {
     @Bean
-    public Docket SwaggerApi() {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(swaggerInfo()) //  API Docu 및 작성자 정보 매핑
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.a604.cake4u.controller"))
-                .paths(PathSelectors.any()) //  controller package 전부
-//                  .paths(PathSelectors.ant("/controller")) //  controller package만 택해서 할 수도 있다.
+                .apis(RequestHandlerSelectors.basePackage("com.a604.cake4u"))
+                .paths(PathSelectors.any())
                 .build()
-                .useDefaultResponseMessages(false); //  기본 세팅값인 200, 401, 402, 403, 404를 사용하지 않는다.
+                ;
     }
 
-    private ApiInfo swaggerInfo() {
-        return new ApiInfoBuilder().title("Spring API Documentation")
-                .description("앱 서버 API를 설명하기 위한 문서입니다.")
-                .version("1")
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
                 .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization ", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization ", "X-AUTH-TOKEN", "header");
     }
 }
