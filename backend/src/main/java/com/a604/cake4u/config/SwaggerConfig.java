9520cffc5946f3902d40e6cb9766e1be384d5f6c
unknown <seongmin42@naver.com>
2023-03-20 12:35:13 +0900

    Misc: Merging develop
    Former-commit-id: 26393948260cbda1d4b1e6e194f049949041f0ab

--- backend/src/main/java/com/a604/cake4u/config/SwaggerConfig.java
+++ backend/src/main/java/com/a604/cake4u/config/SwaggerConfig.java
@@ -1,33 +1,27 @@
 package com.a604.cake4u.config;
 
-import java.util.Arrays;
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket api() {
+    public Docket SwaggerApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
+                .apiInfo(swaggerInfo()) //  API Docu 및 작성자 정보 매핑
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.a604.cake4u"))
                 .paths(PathSelectors.any()) //  controller package 전부
-                .build();
+                .build()
+                .useDefaultResponseMessages(false); //  기본 세팅값인 200, 401, 402, 403, 404를 사용하지 않는다.
     }
 
     private ApiInfo swaggerInfo() {
@@ -37,14 +31,4 @@ public class SwaggerConfig {
                 .build();
     }
 
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("Authorization ", authorizationScopes));
-    }
-
-    private ApiKey apiKey() {
-        return new ApiKey("Authorization ", "X-AUTH-TOKEN", "header");
-    }
 }
