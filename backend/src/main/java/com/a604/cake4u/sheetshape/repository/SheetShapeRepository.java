e9c1ad5f9d2743f858779aeedca3b650c16c091e
최은녕 <djflsth@naver.com>
2023-03-20 17:49:17 +0900

    Feat: SheetTaste, size, shape 구현
    Former-commit-id: 98f114b5af2ece46c3be8cfb7b8ce037688e7a5e

--- backend/src/main/java/com/a604/cake4u/sheetshape/repository/SheetShapeRepository.java
+++ backend/src/main/java/com/a604/cake4u/sheetshape/repository/SheetShapeRepository.java
@@ -1,7 +1,17 @@
 package com.a604.cake4u.sheetshape.repository;
 
+import com.a604.cake4u.sheetshape.dto.SheetShapeResponseDto;
+import com.a604.cake4u.sheetshape.entity.SheetShape;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SheetShapeRepository {
+public interface SheetShapeRepository extends JpaRepository<SheetShape, Long> {
+    @Query("select new com.a604.cake4u.sheetshape.dto.SheetShapeResponseDto(s) " +
+            "from SheetShape s " +
+            "where s.sellerId=:sellerId")
+    SheetShapeResponseDto findSheetShape(@Param("sellerId") Long sellerId);
+
 }
