5b36f2faef0979c432351b8c0856128e3abac0d8
최은녕 <djflsth@naver.com>
2023-03-21 10:47:57 +0900

    Feat: CreamTaste 구현, seller 회원가입 시 시트맛, 크림, 모양, 크림맛 초기화되어 DB에 들어가도록 구현
    Former-commit-id: b425c92f03ea19a17e37ce8e712922078642f16d

--- backend/src/main/java/com/a604/cake4u/creamtaste/dto/CreamTasteResponseDto.java
+++ backend/src/main/java/com/a604/cake4u/creamtaste/dto/CreamTasteResponseDto.java
@@ -1,12 +1,32 @@
 package com.a604.cake4u.creamtaste.dto;
 
+import com.a604.cake4u.creamtaste.entity.CreamTaste;
 import lombok.*;
 
 @Getter
 @Setter
-@Builder
-//@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 public class CreamTasteResponseDto {
+    private Long id;
+    private Long sellerId;
+    private boolean creamCheese;
+    private boolean whippingCream;
+    private boolean chocolateCream;
+    private boolean oreoCream;
+    private boolean matchaCream;
+    private boolean blackSeasameCream;
+    private boolean sweetPotatoCream;
+    private boolean earlGrayCream;
+
+    public CreamTasteResponseDto(CreamTaste entity) {
+        this.id = entity.getId();
+        this.sellerId = entity.getSellerId();
+        this.creamCheese = entity.isCreamCheese();
+        this.whippingCream = entity.isWhippingCream();
+        this.chocolateCream = entity.isChocolateCream();
+        this.oreoCream = entity.isOreoCream();
+        this.matchaCream = entity.isMatchaCream();
+        this.blackSeasameCream = entity.isBlackSeasameCream();
+        this.sweetPotatoCream = entity.isSweetPotatoCream();
+        this.earlGrayCream = entity.isEarlGrayCream();
+    }
 }
