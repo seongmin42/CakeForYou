8724beb46b03cb68c382e4eabc3d49a3d0adb820
forest7592 <forest7592@hanmail.net>
2023-03-22 01:39:52 +0900

    Feat: Add Login form
    Former-commit-id: 213d99d3747f2497020d6c919ea71776649d9c5a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./Home";
 import Login from "./Login";
-import Drag from "./Drag";
 import DragSize from "./DragSize";
-import Wtest from "./Wtest";
 
 function App() {
   const AppContainer = styled.div`
@@ -16,12 +15,11 @@ function App() {
   `;
   return (
     <AppContainer>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/drag" element={<Drag />} />
         <Route path="/dragsize" element={<DragSize />} />
-        <Route path="/wtest" element={<Wtest />} />
       </Routes>
     </AppContainer>
   );
--- frontend/src/components/Hb7.js
+++ frontend/src/components/Hb7.js
@@ -1,13 +1,8 @@
 import styled from "styled-components";
 
-const H7 = styled.div`
-  font-size: ${(props) => {
-    if (props.fontsize) {
-      return props.fontsize;
-    }
-    return "20px";
-  }};
-  font-weight: normal;
+const Hb7 = styled.div`
+  font-size: 20px;
+  font-weight: bold;
   font-family: ${(props) => {
     if (props.font) {
       return props.font;
@@ -15,9 +10,9 @@ const H7 = styled.div`
     if (props.theme && props.theme.font) {
       return props.theme.font;
     }
-    return "NanumGothic";
+    return "Roboto";
   }};
   color: ${(props) => (props.color ? props.color : "black")};
 `;
 
-export default H7;
+export default Hb7;
