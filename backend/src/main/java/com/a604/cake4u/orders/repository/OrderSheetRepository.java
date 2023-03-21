e723841e1f61af40aa5128ef8dc260ef12f0f071
허재성 <cork2586@naver.com>
2023-03-21 14:30:04 +0900

    Fix: 주문 상태에 따른 조회 API 추가
    Former-commit-id: b4ae0f18fbd02a717259f787a0da68791ab9448c

--- backend/src/main/java/com/a604/cake4u/orders/repository/OrderSheetRepository.java
+++ backend/src/main/java/com/a604/cake4u/orders/repository/OrderSheetRepository.java
@@ -1,10 +1,12 @@
 package com.a604.cake4u.orders.repository;
 
+import com.a604.cake4u.enums.EStatus;
 import com.a604.cake4u.orders.entity.OrderSheet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import springfox.documentation.spring.data.rest.EntityAssociationSaveExtractor;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,4 +20,10 @@ public interface OrderSheetRepository extends JpaRepository<OrderSheet, Long> {
     //  판매자 id로 해당 판매자의 모든 주문 찾음
     @Query(value = "select o from OrderSheet o where o.seller.id = :sellerId")
     Optional<List<OrderSheet>> findAllOrderSheetBySellerId(@Param("sellerId") Long sellerId);
+    
+    //  구매자의 주문 상태에 따른 검색
+    Optional<List<OrderSheet>> findOrderSheetsByBuyer_IdAndStatus(Long buyerId, EStatus status);
+
+    //  판매자의 주문 상태에 따른 검색
+    Optional<List<OrderSheet>> findOrderSheetsBySeller_IdAndStatus(Long sellerId, EStatus status);
 }
