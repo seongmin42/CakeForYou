dee912cf8a7daa3d65ae9bf277e9c529d2e5832d
forest7592 <forest7592@hanmail.net>
2023-03-21 21:57:33 +0900

    Misc: Modify number for check jenkins
    Former-commit-id: 0c9463ce2febb87493d05b5c3d0364e68d592511

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -144,7 +144,7 @@ function Login() {
             color="#E79CB3"
             style={{ position: "absolute", left: 314, bottom: 129 }}
           >
-            123단 케이크
+            777단 케이크
           </B5>
           <B7
             color="white"
