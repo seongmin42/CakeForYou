2cdbf0229ba1e7eb8c02da2127277c8b9394ca01
문성현 <forest7592@hanmail.net>
2023-03-21 17:49:35 +0900

    Update Login.jsx
    Former-commit-id: 8b0b583c40cd57289476a47488a1315ccaa38b5e

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -119,7 +119,7 @@ function Login() {
             color="#E79CB3"
             style={{ position: "absolute", left: 314, bottom: 129 }}
           >
-            3단 케이크
+            2단 케이크
           </B5>
           <B7
             color="white"
