19a1e9571d073cacea7cb62429d48ad78d3c51b7
unknown <seongmin42@naver.com>
2023-03-23 16:50:14 +0900

    Feat: Add Wishlist based Recommendation
    Former-commit-id: 41756af5d3f7ecf07e68abc898db41d8b25f0f1e

--- backend/src/main/java/com/a604/cake4u/recommendation/service/RecommendationService.java
+++ backend/src/main/java/com/a604/cake4u/recommendation/service/RecommendationService.java
@@ -1,14 +1,18 @@
 package com.a604.cake4u.recommendation.service;
 
 import com.a604.cake4u.enums.EGender;
+import com.a604.cake4u.enums.ESituation;
 import com.a604.cake4u.portfolio.dto.PortfolioResponseDto;
 import com.a604.cake4u.portfolio.entity.Portfolio;
 import com.a604.cake4u.portfolio.repository.PortfolioRepository;
 import com.a604.cake4u.portfolio.service.PortfolioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +21,9 @@ import java.util.List;
 @RequiredArgsConstructor
 @Transactional
 public class RecommendationService {
+
+    @Value("${flask.baseurl}")
+    private String baseurl;
     @Autowired
     private PortfolioRepository portfolioRepository;
     @Autowired
@@ -31,5 +38,29 @@ public class RecommendationService {
         }
         return portfolioDtos;
     };
+
+    public List<PortfolioResponseDto> getPortfolioRecommendationBySituation(ESituation situation){
+        List<Portfolio> portfolios = portfolioRepository.findPortfolioBySituationOrderByHitDesc(situation);
+        List<PortfolioResponseDto> portfolioDtos = new ArrayList<>();
+        for (Portfolio p : portfolios){
+            portfolioDtos.add(portfolioService.portfolioEntityToPortfolioResponseDTO(p));
+        }
+        return portfolioDtos;
+    }
+
+    @Async
+    @Transactional
+    public List<PortfolioResponseDto> getPortfolioRecommendationByWishlist(Long userId){
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<PortfolioResponseDto> recommendPortfolios = new ArrayList<>();
+        String url = baseurl + "/db/" + userId;
+        Long[] recommends = restTemplate.getForObject(url, Long[].class);
+        for(Long recommend : recommends){
+            recommendPortfolios.add(portfolioService.portfolioEntityToPortfolioResponseDTO(portfolioRepository.findById(recommend).get()));
+        }
+
+        return recommendPortfolios;
+    }
 
 }
