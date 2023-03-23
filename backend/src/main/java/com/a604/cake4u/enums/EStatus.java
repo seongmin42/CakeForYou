b02ff2b01b1a0d926a9ca19a0d13069c787b88ac
허재성 <cork2586@naver.com>
2023-03-23 10:32:00 +0900

    Fix: 주문서 controller PathVariable에 name 속성 추가, 주문 상태 변경 API 추가, Status Enum 타입 이름 수정
    Former-commit-id: 6998313ec19f7102bedeaafde3b686b246b266ab

--- backend/src/main/java/com/a604/cake4u/enums/EStatus.java
+++ backend/src/main/java/com/a604/cake4u/enums/EStatus.java
@@ -1,6 +1,6 @@
 package com.a604.cake4u.enums;
 
-public enum EStatus {
-    REGISTRATION, SEND, DEPOSITCOMPLETE, FINISHPICKUP
+public enum  EStatus {
+    REGISTRATION, SEND, DEPOSIT_COMPLETE, FINISH_PICK_UP
     //주문서등록, 견적서 발송, 입금 완료, 픽업 완료
 }
