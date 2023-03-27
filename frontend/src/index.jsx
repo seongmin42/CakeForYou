eff6923fb7ba92e79718d218d27e234b62870e88
unknown <seongmin42@naver.com>
2023-03-27 17:45:13 +0900

    Feat: Add Login Feature in Frontend Header
    Former-commit-id: da451cb349c4d85624594d622b7d158f6f6f83c6

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import GlobalStyle from "./global";
+import store from "./store/store";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <GlobalStyle />
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
