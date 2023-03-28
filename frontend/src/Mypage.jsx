9c222ed4138965ba0d0f79b6a007270630136406
forest7592 <forest7592@hanmail.net>
2023-03-29 02:00:55 +0900

    Feat: Add stable diffusion
    Former-commit-id: 1637ba4352771cceded35052240debf339c2f9a6 [formerly 7215bcfeaaade1f3e36efd3239a06bd3d64e1d26]
    Former-commit-id: b710a0444633d8cf86dbe0ac78bd0d2edda1067a

--- frontend/src/Mypage.jsx
+++ frontend/src/Mypage.jsx
@@ -1,21 +1,56 @@
 import React from "react";
+import Header from "./components/Header";
 import UpDownContainer from "./components/layout/UpDownContainer";
 import RowContainer from "./components/layout/RowContainer";
+import ColContainer from "./components/layout/ColContainer";
 import Burger from "./assets/img/burger.png";
+import GapH from "./components/layout/GapH";
+import GapW from "./components/layout/GapW";
+import BoldMedium from "./components/text/BoldMedium";
+import BoldLarge from "./components/text/BoldLarge";
+import Button4 from "./components/button/Button4";
 
 function Mypage() {
   return (
-    <UpDownContainer>
-      <RowContainer width="353px">
-        <img
-          src={Burger}
-          alt="burger"
-          style={{
-            width: "79px",
-          }}
-        />
-      </RowContainer>
-    </UpDownContainer>
+    <div>
+      <Header />
+      <UpDownContainer>
+        <RowContainer
+          justify="start"
+          height="calc(100vh * 29.8 / 100)"
+          background="#F0F0E8"
+        >
+          <ColContainer width="18.3%" justify="start">
+            <GapH height="30.8%" />
+            <img
+              src={Burger}
+              alt="burger"
+              style={{
+                width: "79px",
+              }}
+            />
+          </ColContainer>
+          <ColContainer
+            width="81.7%"
+            height="100%"
+            justify="start"
+            align="start"
+          >
+            <GapH height="22.3%" />
+            <BoldMedium>MY PAGE</BoldMedium>
+            <GapH height="15.4%" />
+            <RowContainer justify="start">
+              <BoldLarge>배진호 님의 마이페이지입니다</BoldLarge>
+              <GapW width="10.1%" />
+              <Button4>
+                <BoldMedium color="white">회원정보 수정</BoldMedium>
+              </Button4>
+            </RowContainer>
+          </ColContainer>
+        </RowContainer>
+        <RowContainer height="calc(100vh * 70.2 / 100 - 60px)">a</RowContainer>
+      </UpDownContainer>
+    </div>
   );
 }
 
---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/sdapi",
+    createProxyMiddleware({
+      target: "http://127.0.0.1:7861",
+      changeOrigin: true,
+    })
+  );
+};
