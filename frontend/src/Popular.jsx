875c4f652df6d71dce09c1e4d1ef4498c9ef642a
문성현 <forest7592@hanmail.net>
2023-03-28 10:12:43 +0900

    Fix: Fix popular page
    Former-commit-id: beeb0a23038ad00e37b27d5b3f59a8b564047dc9 [formerly 009774239284466adac03684579c12c158efc245]
    Former-commit-id: ee55ef56af5a624d82ddc8b2827a68172709ceb0

--- frontend/src/Popular.jsx
+++ frontend/src/Popular.jsx
@@ -1,34 +1,65 @@
 import React from "react";
-// import styled from "styled-components";
+import Header from "./components/Header";
 import UpDownContainer from "./components/layout/UpDownContainer";
 import RowContainer from "./components/layout/RowContainer";
 import GapH from "./components/layout/GapH";
 import SmallMedium from "./components/text/SmallMedium";
 import BoldSmallMedium from "./components/text/BoldSmallMedium";
 import Card from "./components/Card";
+import PinkSearch from "./assets/img/pink_search.png";
 
 function Popular() {
   return (
-    <UpDownContainer align="center">
-      <GapH height="34px" />
-      <RowContainer
-        height="61px"
-        width="451px"
-        border="1px solid"
-        borderRadius="30px"
-        borderColor="#E3E3E3"
-      >
-        <SmallMedium>내 지역 선택</SmallMedium>
-      </RowContainer>
-      <GapH height="24px" />
-      <RowContainer width="1190px" justify="start">
-        <BoldSmallMedium>인기 케이크</BoldSmallMedium>
-      </RowContainer>
-      <GapH height="24px" />
-      <RowContainer>
-        <Card />
-      </RowContainer>
-    </UpDownContainer>
+    <div>
+      <Header />
+      <UpDownContainer align="center">
+        <GapH height="34px" />
+        <RowContainer
+          height="61px"
+          width="451px"
+          border="1px solid"
+          borderRadius="30px"
+          borderColor="#E3E3E3"
+          position="relative"
+        >
+          <SmallMedium>내 지역 선택</SmallMedium>
+          <img
+            src={PinkSearch}
+            alt="img"
+            style={{
+              width: "50px",
+              position: "absolute",
+              right: "26px",
+            }}
+          />
+        </RowContainer>
+        <GapH height="24px" />
+        <RowContainer width="1194px" justify="start">
+          <BoldSmallMedium>인기 케이크</BoldSmallMedium>
+        </RowContainer>
+        <GapH height="24px" />
+        <RowContainer width="1194px" gap="21px">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </RowContainer>
+        <GapH height="40px" />
+        <RowContainer width="1194px" justify="start">
+          <BoldSmallMedium>인기 가게</BoldSmallMedium>
+        </RowContainer>
+        <GapH height="24px" />
+        <RowContainer width="1194px" gap="21px">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </RowContainer>
+        <GapH height="83px" />
+      </UpDownContainer>
+    </div>
   );
 }
 
