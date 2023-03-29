5fe90291fc3d64dcc9f709e67b409582928f1adb
unknown <seongmin42@naver.com>
2023-03-29 17:57:13 +0900

    Misc
    Former-commit-id: d27978c5bd8b64059d04243d83a890fa15450d73 [formerly 805f37a0e854cdd2980b356b8ff62289910ef0e9]
    Former-commit-id: 457624c2ea3e13832d16ce7c1d7f4e776f426930

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import { Routes, Route } from "react-router-dom";
+import Welcome from "./Welcome";
+import Login from "./Login";
+import Main from "./Main";
+import DragSize from "./DragSize";
+import AuthPage from "./AuthPage";
+import SignUpBuyer from "./SignUpBuyer";
+import SignUpSeller from "./SignUpSeller";
+import RecommendPersonal from "./RecommendPersonal";
+import InfoDetail from "./InfoDetail";
+import RecommendTheme from "./RecommendTheme";
+import Testpage from "./test/Testpage";
+import Popular from "./Popular";
+import MyPage from "./MyPage";
+import PortfolioModal from "./components/PortfolioModal";
+import Diff from "./Diff";
+import MakeOrder from "./MakeOrder";
+import RecommendWishList from "./RecommendWishlist";
+
+const AppContainer = styled.div`
+  &,
+  & * {
+    box-sizing: border-box;
+  }
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup/buyer" element={<SignUpBuyer />} />
+        <Route path="/signup/seller" element={<SignUpSeller />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/popular" element={<Popular />} />
+        <Route path="/modal" element={<PortfolioModal />} />
+        <Route path="/myPage" element={<MyPage />} />
+        <Route path="/diff" element={<Diff />} />
+        <Route path="/makeOrder" element={<MakeOrder />} />
+        <Route path="/test" element={<Testpage />} />
+        <Route path="/dragsize" element={<DragSize />} />
+        <Route path="/oauth/redirect" element={<AuthPage />} />
+        <Route path="/recommend/personal" element={<RecommendPersonal />} />
+        <Route path="/infodetail" element={<InfoDetail />} />
+        <Route path="/recommend/theme" element={<RecommendTheme />} />
+        <Route path="/recommend/wishlist" element={<RecommendWishList />} />
+      </Routes>
+    </AppContainer>
+  );
+}
+
+export default App;
