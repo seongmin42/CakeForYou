53dccd01cffc5ef889a66337df56780b291ebddb
unknown <seongmin42@naver.com>
2023-03-29 17:34:45 +0900

    Feat: Add Seller Security Login
    Former-commit-id: be938f8969159dea2ad4744fb2df54dfdd5706b9 [formerly ddd891d036db454f2b148ff62f2bea1445681abd]
    Former-commit-id: d6e5691feb0c5d7e22e0fbaff93bbd2a846145e4

--- backend/src/main/java/com/a604/cake4u/auth/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/a604/cake4u/auth/service/CustomUserDetailsService.java
@@ -6,6 +6,8 @@ import com.a604.cake4u.buyer.entity.Buyer;
 import com.a604.cake4u.buyer.repository.BuyerRepository;
 import com.a604.cake4u.exception.BaseException;
 import com.a604.cake4u.exception.ErrorMessage;
+import com.a604.cake4u.seller.entity.Seller;
+import com.a604.cake4u.seller.repository.SellerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,17 +21,26 @@ import java.util.Optional;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final BuyerRepository buyerRepository;
+    private final SellerRepository sellerRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Optional<Buyer> buyer = buyerRepository.findByEmail(email);
-        if (!buyer.isPresent()) {
-            throw new BaseException(ErrorMessage.NOT_EXIST_EMAIL);
+        Optional<Seller> seller = sellerRepository.findByEmail(email);
+
+
+        if(buyer.isPresent()){
+            return UserPrincipal.createBuyerPrincipal(buyer.get());
+        }
+
+        if(seller.isPresent()){
+            return UserPrincipal.createSellerPrincipal(seller.get());
         }
 
         else if("관리자".equals(buyer.get().getNickname())) {
         	return UserPrincipal.createAdmin(buyer.get());
         }
-        return UserPrincipal.createBuyerPrincipal(buyer.get());
+
+        throw new BaseException(ErrorMessage.NOT_EXIST_EMAIL);
     }
 }
