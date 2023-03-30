b46af3df52965981b6b4fd21a79924eaafdae987
문성현 <forest7592@hanmail.net>
2023-03-30 10:32:12 +0900

    Fix: Fix axiosInstance
    Former-commit-id: 8a6827ef1ca0e8afe9cad623d3a7d92a9fe061b9 [formerly d480fd695ca395cb3d89ecbc57af2a835f621137]
    Former-commit-id: d8aeb20ead556b6de58975f81ebd1fea34966116

--- frontend/src/util/axiosInstance.js
+++ frontend/src/util/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: "https://j8a604.p.ssafy.io/api/",
 });
 
 axiosInstance.interceptors.request.use(
