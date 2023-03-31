8bd2d3953f590cb40459c92a9636c56256727fcc
최은녕 <djflsth@naver.com>
2023-03-31 11:14:13 +0900

    fix: sheetsize,taste jpa 관련 쿼리문 수정
    Former-commit-id: ff12432f452885bcd4bc0748f96c3ac120b468fe [formerly 78e082240ed407530a88e8641a62470b3a59137d]
    Former-commit-id: 4b8bc115f1a0f3c3f96fa18f570376c2a15aa8c4

--- backend/src/main/java/com/a604/cake4u/sheettaste/repository/SheetTasteRepository.java
+++ backend/src/main/java/com/a604/cake4u/sheettaste/repository/SheetTasteRepository.java
@@ -4,6 +4,7 @@ import com.a604.cake4u.sheettaste.dto.SheetTasteResponseDto;
 import com.a604.cake4u.sheettaste.entity.SheetTaste;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,5 +12,5 @@ public interface SheetTasteRepository extends JpaRepository<SheetTaste, Long> {
     @Query("select new com.a604.cake4u.sheettaste.dto.SheetTasteResponseDto(s) " +
             "from SheetTaste s " +
             "where s.sellerId =:sellerId")
-    SheetTasteResponseDto findSheetTaste(Long sellerId);
+    SheetTasteResponseDto findSheetTaste(@Param("sellerId") Long sellerId);
 }
