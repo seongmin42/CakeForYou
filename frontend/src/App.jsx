4e5bc767c441f51a43b3a8499529010be56e4c03
forest7592 <forest7592@hanmail.net>
2023-04-03 06:44:50 +0900

    Feat: Add order modal, seller portfolio regist
    Former-commit-id: 11153f6357a9985bf11b71abb442320ab6c348d1 [formerly 5d0c6baca47f33d1867a3c989a87ed028d74e19d]
    Former-commit-id: 5e97beb7bdf03ccc502e8357ddd6e4640fcfecf8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,6 +23,7 @@ import SellerInfo from "./SellerInfo";
 import SellerCustom from "./SellerCustom";
 import SellerOrder from "./SellerOrder";
 import OrderModal from "./components/OrderModal";
+import SellerPortfolioRegist from "./SellerPortfolioRegist";
 
 const AppContainer = styled.div`
   &,
@@ -57,6 +58,10 @@ function App() {
         <Route path="/seller/info" element={<SellerInfo />} />
         <Route path="/seller/custom" element={<SellerCustom />} />
         <Route path="/seller/order" element={<SellerOrder />} />
+        <Route
+          path="/seller/portfolio/regist"
+          element={<SellerPortfolioRegist />}
+        />
         <Route path="/orderModal" element={<OrderModal />} />
       </Routes>
     </AppContainer>
