545c5d3281cf939385ca6ca659ebe49594bf1a35
문성현 <forest7592@hanmail.net>
2023-04-06 13:22:08 +0900

    Feat: Add assets to cakeDiy
    Former-commit-id: 225de9a30d47137d555a624e90e65ec8db410939 [formerly 06a3748ad8fb9d9d4862c7e0e2875fc11e7d2ce0]
    Former-commit-id: 359f6be416a7bb145c36eadbc6e1cd517e5e45ae

--- frontend/src/components/SidebarDiy.jsx
+++ frontend/src/components/SidebarDiy.jsx
@@ -11,10 +11,30 @@ import Cream6 from "../assets/img/cream6.png";
 import Cream7 from "../assets/img/cream7.png";
 import Cream8 from "../assets/img/cream8.png";
 import Deco1 from "../assets/img/deco1.png";
+import Deco2 from "../assets/img/deco2.png";
+import Deco3 from "../assets/img/deco3.png";
+import Deco4 from "../assets/img/deco4.png";
+import Deco5 from "../assets/img/deco5.png";
+import Deco6 from "../assets/img/deco6.png";
+import Deco7 from "../assets/img/deco7.png";
+import Deco8 from "../assets/img/deco8.png";
+import Deco9 from "../assets/img/deco9.png";
+import Deco10 from "../assets/img/deco10.png";
 import Char1 from "../assets/img/char1.png";
 import Char2 from "../assets/img/char2.png";
 import Char3 from "../assets/img/char3.png";
 import Char4 from "../assets/img/char4.png";
+import Char5 from "../assets/img/char5.png";
+import Char6 from "../assets/img/char6.png";
+import Char7 from "../assets/img/char7.png";
+import Char8 from "../assets/img/char8.png";
+import Char9 from "../assets/img/char9.png";
+import Char10 from "../assets/img/char10.png";
+import Char11 from "../assets/img/char11.png";
+import Char12 from "../assets/img/char12.png";
+import Char13 from "../assets/img/char13.png";
+import Char14 from "../assets/img/char14.png";
+import Char15 from "../assets/img/char15.png";
 import Orange1 from "../assets/img/orange1.png";
 import Orange2 from "../assets/img/orange2.png";
 import Orange3 from "../assets/img/orange3.png";
@@ -66,7 +86,10 @@ function SidebarDiy({ onImageClick }) {
     <SidebarContainer>
       <RowContainer>
         <Button1
-          background="rgb(181 199 211)"
+          background="rgb(255 247 224)"
+          color="black"
+          borderRight="1px solid #cccccc"
+          borderBottom="1px solid #cccccc"
           borderRadius="0"
           width="75px"
           type="button"
@@ -76,15 +99,21 @@ function SidebarDiy({ onImageClick }) {
         </Button1>
         <Button1
           background="rgb(255 247 224)"
+          borderRight="1px solid #cccccc"
+          borderBottom="1px solid #cccccc"
           borderRadius="0"
           width="75px"
           color="black"
           type="button"
           onClick={() => setActiveTab(2)}
         >
-          크림2
+          데코
         </Button1>
         <Button1
+          background="rgb(255 247 224)"
+          color="black"
+          borderRight="1px solid #cccccc"
+          borderBottom="1px solid #cccccc"
           borderRadius="0"
           width="75px"
           type="button"
@@ -93,8 +122,10 @@ function SidebarDiy({ onImageClick }) {
           동물
         </Button1>
         <Button1
-          background="rgb(255 111 97)"
+          background="rgb(255 247 224)"
+          color="black"
           borderRadius="0"
+          borderBottom="1px solid #cccccc"
           width="75px"
           type="button"
           onClick={() => setActiveTab(4)}
@@ -119,7 +150,15 @@ function SidebarDiy({ onImageClick }) {
       {activeTab === 2 && (
         <>
           <StyledImage src={Deco1} onClick={() => onImageClick(Deco1)} />
-          <StyledImage src={Deco1} onClick={() => onImageClick(Deco1)} />
+          <StyledImage src={Deco2} onClick={() => onImageClick(Deco2)} />
+          <StyledImage src={Deco3} onClick={() => onImageClick(Deco3)} />
+          <StyledImage src={Deco4} onClick={() => onImageClick(Deco4)} />
+          <StyledImage src={Deco5} onClick={() => onImageClick(Deco5)} />
+          <StyledImage src={Deco6} onClick={() => onImageClick(Deco6)} />
+          <StyledImage src={Deco7} onClick={() => onImageClick(Deco7)} />
+          <StyledImage src={Deco8} onClick={() => onImageClick(Deco8)} />
+          <StyledImage src={Deco9} onClick={() => onImageClick(Deco9)} />
+          <StyledImage src={Deco10} onClick={() => onImageClick(Deco10)} />
         </>
       )}
       {activeTab === 3 && (
@@ -128,6 +167,17 @@ function SidebarDiy({ onImageClick }) {
           <StyledImage src={Char2} onClick={() => onImageClick(Char2)} />
           <StyledImage src={Char3} onClick={() => onImageClick(Char3)} />
           <StyledImage src={Char4} onClick={() => onImageClick(Char4)} />
+          <StyledImage src={Char5} onClick={() => onImageClick(Char5)} />
+          <StyledImage src={Char6} onClick={() => onImageClick(Char6)} />
+          <StyledImage src={Char7} onClick={() => onImageClick(Char7)} />
+          <StyledImage src={Char8} onClick={() => onImageClick(Char8)} />
+          <StyledImage src={Char9} onClick={() => onImageClick(Char9)} />
+          <StyledImage src={Char10} onClick={() => onImageClick(Char10)} />
+          <StyledImage src={Char11} onClick={() => onImageClick(Char11)} />
+          <StyledImage src={Char12} onClick={() => onImageClick(Char12)} />
+          <StyledImage src={Char13} onClick={() => onImageClick(Char13)} />
+          <StyledImage src={Char14} onClick={() => onImageClick(Char14)} />
+          <StyledImage src={Char15} onClick={() => onImageClick(Char15)} />
         </>
       )}
       {activeTab === 4 && (
