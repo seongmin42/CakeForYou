4696f9b766c097357adf2437a7227690fed3c017
배진호 <qowlsgh99@naver.com>
2023-04-06 13:29:54 +0900

    Fix: 포트폴리오 카드 해시태그 한글화
    Former-commit-id: 2bf74647798804d0605447f6e3fa0798817cf5d8 [formerly 3256bfc88fd125b40b504512b9a86f6310d1a7ce]
    Former-commit-id: 59ddab78b58ee26f958782e962b102624c340fd2

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -30,6 +30,60 @@ function Card({
   hit,
   createdAt,
 }) {
+  // RED, PINK, ORANGE, YELLOW, GREEN, BLUE, PURPLE, BLACK, WHITE, PASTEL, PRINTING
+  if (color === "RED") color = "빨강";
+  else if (color === "PINK") color = "분홍";
+  else if (color === "ORANGE") color = "주황";
+  else if (color === "YELLOW") color = "노랑";
+  else if (color === "GREEN") color = "초록";
+  else if (color === "BLUE") color = "파랑";
+  else if (color === "PURPLE") color = "보라";
+  else if (color === "BLACK") color = "검정";
+  else if (color === "WHITE") color = "하얀";
+  else if (color === "PASTEL") color = "파스텔";
+  else if (color === "PRINTING") color = "프린팅";
+
+  // VANILLA, CHOCOLATE, EARL_GRAY, RED_VELVET, MATCHA,
+  // MOCHA, CHEESE, CARROT, SWEET_POTATO;
+  if (sheetTaste === "VANILLA") sheetTaste = "바닐라시트";
+  else if (sheetTaste === "CHOCOLATE") sheetTaste = "초콜릿시트";
+  else if (sheetTaste === "EARL_GRAY") sheetTaste = "얼그레이시트";
+  else if (sheetTaste === "RED_VELVET") sheetTaste = "레드벨벳시트";
+  else if (sheetTaste === "MATCHA") sheetTaste = "말차시트";
+  else if (sheetTaste === "MOCHA") sheetTaste = "모카시트";
+  else if (sheetTaste === "CHEESE") sheetTaste = "치즈시트";
+  else if (sheetTaste === "CARROT") sheetTaste = "당근시트";
+  else if (sheetTaste === "SWEET_POTATO") sheetTaste = "고구마시트";
+
+  // CIRCLE, HEART, RECTANGLE, OTHERS;
+  if (shape === "CIRCLE") shape = "원형";
+  else if (shape === "HEART") shape = "하트";
+  else if (shape === "RECTANGLE") shape = "사각";
+  else if (shape === "OTHERS") shape = "기타";
+
+  // CREAM_CHEESE, WHIPPING_CREAM, CHOCOLATE_CREAM, OREO_CREAM, MATCHA_CREAM,
+  // BLACK_SESAME_CREAM, SWEET_POTATO_CREAM, EARL_GRAY_CREAM, STRAWBERRY_CREAM;
+  if (creamTaste === "CREAM_CHEESE") creamTaste = "크림치즈크림";
+  else if (creamTaste === "WHIPPING_CREAM") creamTaste = "생크림";
+  else if (creamTaste === "CHOCOLATE_CREAM") creamTaste = "초콜릿크림";
+  else if (creamTaste === "OREO_CREAM") creamTaste = "오레오크림";
+  else if (creamTaste === "MATCHA_CREAM") creamTaste = "말차크림";
+  else if (creamTaste === "BLACK_SESAME_CREAM") creamTaste = "흑임자크림";
+  else if (creamTaste === "SWEET_POTATO_CREAM") creamTaste = "고구마크림";
+  else if (creamTaste === "EARL_GRAY_CREAM") creamTaste = "얼그레이크림";
+  else if (creamTaste === "STRAWBERRY_CREAM") creamTaste = "딸기크림";
+
+  // IDOL, COMPANY, SIXTIETH, BIRTHDAY, ANNIVERSARY, MARRIAGE, DISCHARGE, CHRISTMAS, ETC
+  if (situation === "IDOL") situation = "아이돌";
+  else if (situation === "COMPANY") situation = "입/퇴사";
+  else if (situation === "SIXTIETH") situation = "환갑";
+  else if (situation === "BIRTHDAY") situation = "생일";
+  else if (situation === "ANNIVERSARY") situation = "기념일";
+  else if (situation === "MARRIAGE") situation = "결혼케이크";
+  else if (situation === "DISCHARGE") situation = "전역";
+  else if (situation === "CHRISTMAS") situation = "크리스마스";
+  else if (situation === "ETC") situation = "기타";
+
   const desc = "#".concat(
     [color, shape, sheetTaste, creamTaste, situation].join(" #")
   );
