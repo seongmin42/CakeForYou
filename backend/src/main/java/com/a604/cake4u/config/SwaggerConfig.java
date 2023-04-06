530ecfdafdae8becfda0240c8ae4d8dde24cc150
unknown <seongmin42@naver.com>
2023-04-06 14:29:33 +0900

    Misc
    Former-commit-id: 3513ee7267e6fa0b441f14eb5f9ca7090e429b3e [formerly c84f8d6240d7b98431d7655590a5fba511c1d2de]
    Former-commit-id: f4b65daa4e6589605999c039a741b42c5c8d83e8

--- backend/src/main/java/com/a604/cake4u/config/SwaggerConfig.java
+++ backend/src/main/java/com/a604/cake4u/config/SwaggerConfig.java
@@ -3,18 +3,27 @@ package com.a604.cake4u.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket SwaggerApi() {
+
+        List<Parameter> global = new ArrayList<>();
+        global.add(new ParameterBuilder().name("Authorization").description("access-token").parameterType("header").required(false).modelRef(new ModelRef("string")).build());
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(swaggerInfo()) //  API Docu 및 작성자 정보 매핑
                 .select()
